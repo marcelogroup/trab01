8ea5d67147e0bfb77a849951998112354359473f
MarceloMendes94 <marceloestudoifes@gmail.com>
2018-10-14 15:38:17 -0300

    Update Psicultura/src/psicultura/HistoricoMedicao.java

--- Psicultura/src/psicultura/HistoricoMedicao.java
+++ Psicultura/src/psicultura/HistoricoMedicao.java
@@ -14,9 +14,15 @@ import java.util.Date;
 public class HistoricoMedicao {
     public Date data_medicao;
     public Tanque tanque;
+    public float ph;
+    public float oxi;
+    public float temp;
+            
     
     public void historico(Tanque tanque,Date data_medicao){
         this.data_medicao=data_medicao;
-        this.tanque=tanque;
+        this.oxi=tanque.sensor_oxi;
+        this.temp=tanque.sensor_temp;
+        this.ph=tanque.sensor_ph;
     }
 }
